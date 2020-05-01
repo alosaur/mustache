3205b455e634865e1f5a5203e3238848d90d9873
jamaks <jamakrus@gmail.com>
2020-05-01 23:56:47 +0300

    Added render function

--- mod.ts
+++ mod.ts
@@ -3,3 +3,7 @@ import mustache from './mustache.mjs';
 export async function renderFile(path: string, model: Object | undefined) {
     return mustache.render(new TextDecoder().decode(await Deno.readFile(path)), model);
 }
+
+export function render(body: string, model: Object | undefined) {
+    return mustache.render(body, model);
+}
