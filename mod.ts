0b03695965a33c8dcab35862a4aa625a683b15c5
irustm <jamakrus@gmail.com>
2021-02-07 21:22:37 +0300

    Update to std 0.85.0

--- mod.ts
+++ mod.ts
@@ -1,9 +1,13 @@
 import mustache from './mustache.mjs';
 
+const decoder = new TextDecoder();
+
 export async function renderFile(path: string, model: Object | undefined) {
-    return mustache.render(new TextDecoder().decode(await Deno.readFile(path)), model);
+    // @ts-ignore
+    return mustache.render(decoder.decode(await Deno.readFile(path)), model);
 }
 
 export function render(body: string, model: Object | undefined) {
+    // @ts-ignore
     return mustache.render(body, model);
 }
